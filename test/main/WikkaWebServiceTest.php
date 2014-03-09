e8685003b13fb444d7f0a4e9184213d86e630179
klenwell <klenwell@gmail.com>
2014-03-08 20:48:48 -0800

    stubs out WikkaWebService and unit test; updates wikka-refactored.php comments

---
+++ test/main/WikkaWebServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * main/WikkaWebServiceTest.php
+ * 
+ * A test of the WikkaWebService class
+ *
+ * Usage (run from WikkaWiki root dir):
+ * > phpunit --stderr test/main/WikkaWebServiceTest
+ *
+ * NOTE: must run with --stderr to avoid error:
+ *  session_start(): Cannot send session cookie
+ *
+ * To run all tests:
+ * > phpunit --stderr test
+ */
+require_once('wikka/web_service.php');
+
+
+class WikkaModulesTest extends PHPUnit_Framework_TestCase {
+ 
+    /**
+     * Test Fixtures
+     */
+    public static function setUpBeforeClass() {
+    }
+ 
+    public static function tearDownAfterClass() {       
+    }
+    
+    public function setUp() {
+        $this->web_service = new WikkaWebService();
+    }
+    
+    public function tearDown() {
+        $this->web_service = null;
+    }
+    
+    /**
+     * Tests
+     */
+    public function testInstantiation() {
+        $this->assertInstanceOf('WikkaWebService', $this->web_service);
+    }
+}
---
+++ wikka/web_service.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * web_service.php
+ *
+ * The WikkaWebService class orchestrates the response to each request. It
+ * does so by build a request object, routing the request to the appropriate
+ * handler, and then returning a response object that sends the response
+ * back to the browser.
+ *
+ * @package     Wikka
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @author      {@link https://github.com/klenwell/WikkaWiki Tom Atwell}
+ * @copyright   Copyright 2014  Tom Atwell <klenwell@gmail.com>
+ *  
+ */
+
+class WikkaWebService {
+
+    /*
+     * Constructor
+     */
+    public function __construct() {
+    }
+    
+    /*
+     * Public Methods
+     */
+    public function process_request() {
+    }
+    
+    /*
+     * Private Methods
+     */
+}
