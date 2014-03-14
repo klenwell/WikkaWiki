a18db4a08145ab21c3e4984e5d4ec47a7b0c7c1f
klenwell <klenwell@gmail.com>
2014-03-13 22:05:52 -0700

    updates head comment in wikka.php

--- wikka.php
+++ wikka.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * wikka-refactored.php
- *
- * This is a transitional script which should eventually replace wikka.php.
+ * wikka.php
  *
  * This is the main Wikka script. This file is called each time a request is
  * made from the browser.
  *
- * @author    {@link https://github.com/klenwell Tom Atwell}
+ * @author    {@link http://www.mornography.de/ Hendrik Mans}
+ * @author    {@link http://wikkawiki.org/JsnX Jason Tourtelotte}
+ * @author    {@link http://wikkawiki.org/JavaWoman Marjolein Katsma}
+ * @author    {@link http://wikkawiki.org/NilsLindenberg Nils Lindenberg}
+ * @author    {@link http://wikkawiki.org/DotMG Mahefa Randimbisoa}
+ * @author    {@link http://wikkawiki.org/DarTar Dario Taraborelli}
+ * @author    {@link http://wikkawiki.org/BrianKoontz Brian Koontz}
+ * @author    {@link http://wikkawiki.org/TormodHaugen Tormod Haugen}
+ * @author    {@link https://github.com/klenwell/WikkaWiki Tom Atwell}
  *
- * @copyright Copyright 2014, Tom Atwell <klenwell@gmail.com>
- *  
+ * @copyright Copyright 2002-2003, Hendrik Mans <hendrik@mans.de>
+ * @copyright Copyright 2004-2005, Jason Tourtelotte <wikka-admin@jsnx.com>
+ * @copyright Copyright 2006-2014, {@link http://wikkawiki.org/CreditsPage Wikka Development Team}
+ *
  */
 #
 # Imports
