7105947fe813209d2e2d65695e2be6bf23044d02
klenwell <klenwell@gmail.com>
2014-04-10 22:06:31 -0700

    adds comment model and unit test

--- install/schema.php
+++ install/schema.php
@@ -6,6 +6,7 @@
 require_once('models/page.php');
 require_once('models/user.php');
 require_once('models/acl.php');
+require_once('models/comment.php');
 
 
 
@@ -14,7 +15,8 @@ $WikkaDatabaseSchema = array(
 		'SET utf8 COLLATE utf8_unicode_ci',
 	'pages table' => PageModel::get_schema(),
 	'users table' => UserModel::get_schema(),
-	'acls table' => AccessControlListModel::get_schema()
+	'acls table' => AccessControlListModel::get_schema(),
+	'comments table' => CommentModel::get_schema()
 );
 
 $WikkaDatabaseSchema['links table'] = <<<EOC
@@ -40,22 +42,6 @@ $WikkaDatabaseSchema['referrer_blacklist table'] = <<<EOC
 CREATE TABLE {{prefix}}referrer_blacklist (
 	spammer varchar(255) NOT NULL default '',
 	KEY idx_spammer (spammer)
-) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE={{engine}}
-EOC;
-
-$WikkaDatabaseSchema['comments table'] = <<<EOC
-CREATE TABLE {{prefix}}comments (
-	id int(10) unsigned NOT NULL auto_increment,
-	page_tag varchar(75) NOT NULL default '',
-	time datetime NOT NULL default '0000-00-00 00:00:00',
-	comment text NOT NULL,
-	user varchar(75) NOT NULL default '',
-	parent int(10) unsigned default NULL,. 
-	status enum('deleted') default NULL,
-	deleted char(1) default NULL,
-	PRIMARY KEY  (id),
-	KEY idx_page_tag (page_tag),
-	KEY idx_time (time)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE={{engine}}
 EOC;
 
---
+++ models/comment.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * models/comment.php
+ *
+ * WikkaWiki Comment model class.
+ *
+ * @package		Models
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @author      {@link https://github.com/klenwell/WikkaWiki Tom Atwell}
+ * @copyright   Copyright 2014  Tom Atwell <klenwell@gmail.com>
+ *
+ */
+require_once('models/base.php');
+
+ 
+
+class CommentModel extends WikkaModel {
+    /*
+     * Static Properties
+     * (These are just a sample and should be overridden in base class)
+     */
+    protected static $schema = <<<MYSQL
+CREATE TABLE {{prefix}}comments (
+	id int(10) unsigned NOT NULL auto_increment,
+	page_tag varchar(75) NOT NULL default '',
+	time datetime NOT NULL default '0000-00-00 00:00:00',
+	comment text NOT NULL,
+	user varchar(75) NOT NULL default '',
+	parent int(10) unsigned default NULL,. 
+	status enum('deleted') default NULL,
+	deleted char(1) default NULL,
+	PRIMARY KEY  (id),
+	KEY idx_page_tag (page_tag),
+	KEY idx_time (time)
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE={{engine}}
+MYSQL;
+    
+    protected static $table = 'comments';
+    
+}
